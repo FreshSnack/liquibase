a128c6c4e3d2142ea236dbc89368fabd41819d44
mxding <937652174@qq.com>
2021-07-12 22:22:31 +0800

    project init

---
+++ src/main/java/net/snack/liquibase/LiquibaseApplication.java
@@ -0,0 +1,18 @@
+package net.snack.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://blog.csdn.net/liubingyu12345/article/details/78891772
+ * http://cn.voidcc.com/question/p-gcefjtkh-dr.html
+ * https://blog.csdn.net/Reentrant_Lock/article/details/116200821
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApplication.class, args);
+    }
+
+}
